0c8763ae9e7f88977374ad730c2c08a4294434ae
maricruz ruiz <karlamaricruz2904@gmail.com>
2025-04-27 22:10:40 -0600

    Primer commit

---
+++ src/components/head.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+export default function Head(){
+    return(
+        <div className='head contenedor'>
+            <div className="logo-texto">
+                <div className="divlogo">
+                <img src="https://i.pinimg.com/736x/39/3b/b1/393bb1bb15940aea508dd07c5da23917.jpg" alt="logo" className="logoC" />
+                </div>
+                <div className="texto">
+                Café Aura
+                </div>
+            </div>
+            <div>
+                <ul>
+                <li>Inicio</li>
+                <li>Menu</li>
+                <li>Nosotros</li>
+                </ul>
+            </div>  
+        </div>
+
+    )
+}
---
+++ src/pages/home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import '../../src/style.css'
+
+export default function Home(){
+    return(
+        <div> 
+            <h2>Vista home</h2> 
+        </div>
+    )
+}
